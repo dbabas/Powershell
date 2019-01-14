cf592adc2f3a32a5662b0dd6e3932f4b0d403e54
dbabas <dbabas@outlook.com>
2019-01-14 09:18:04 +0200

    Misc
    Misc

---
+++ Office 365/Misc.ps1
@@ -0,0 +1,16 @@
+﻿#Install the module to interact with Office 365
+#Filter with "AzureAD" to see the comdlets
+#Install-Module -Name AzureAD
+
+#Connect to the tenant
+#$credential = Get-Credential
+#Connect-AzureAD -Credential $credential
+
+#Delete a group without waiting 30 days
+#Get-AzureADMSDeletedGroup
+#Remove-AzureADMSDeletedDirectoryObject -Id a5bb6d53-6106-4ca5-bb7e-48ae04714d2e
+
+
+#List of users, groups etc.
+#Get-AzureADUser -All $True
+Get-AzureADGroup
