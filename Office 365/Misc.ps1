a2b268bf5d877a3833f540eb4f6276207378c55b
Dimitrios <dimitrios.bampas@thenavpeople.co.uk>
2019-05-03 10:40:02 +0300

    Updates

--- Office 365/Misc.ps1
+++ Office 365/Misc.ps1
@@ -2,9 +2,12 @@
 #Filter with "AzureAD" to see the comdlets
 #Install-Module -Name AzureAD
 
+Install-Module MSOnline
+
 #Connect to the tenant
-#$credential = Get-Credential
+$credential = Get-Credential
 #Connect-AzureAD -Credential $credential
+
 
 #Delete a group without waiting 30 days
 #Get-AzureADMSDeletedGroup
@@ -13,4 +16,17 @@
 
 #List of users, groups etc.
 #Get-AzureADUser -All $True
-Get-AzureADGroup
+#Get-AzureADGroup
+
+#Rename Group email
+#$credential = Get-Credential
+#Connect-AzureAD -Credential $credential
+#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session -DisableNameChecking
+#Set-UnifiedGroup -Identity “HR” -PrimarySmtpAddress “hr@gerovassiliou.gr”
+#Remove-PSSession $Session #Do not forget to close the session!!!!!!!!!!
+
+#List of users with Last Password Change
+Connect-MsolService -Credential $credential
+Get-MsolUser -All | select DisplayName, LastPasswordChangeTimeStamp
+Get-MsolUser -All | select DisplayName, LastPasswordChangeTimeStamp | Export-CSV LastPasswordChange.csv -NoTypeInformation
