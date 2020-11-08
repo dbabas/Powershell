b9df3e489928fcfec53c8d9d7f89f3d7dde77165
dbabas <32466926+dbabas@users.noreply.github.com>
2020-11-08 21:12:34 +0200

    Update Make Team Calendar Visible in Outlook.ps1

--- Office 365/Make Team Calendar Visible in Outlook.ps1
+++ Office 365/Make Team Calendar Visible in Outlook.ps1
@@ -1,5 +1,5 @@
-﻿#https://techcommunity.microsoft.com/t5/microsoft-teams-ama/why-the-teams-calendar-not-visible-in-outlook/m-p/300155
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-set-UnifiedGroup -identity "TeamNameHere" -HiddenFromExchangeClientsEnabled:$false
+#Connect to exchange on-line
+#https://docs.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+Connect-ExchangeOnline -UserPrincipalName db@all4nav.gr -ShowProgress $true
+#Make team's calendar available in Outlook.
+set-UnifiedGroup -identity "Γενική Διεύθυνση - Τεστ" -HiddenFromExchangeClientsEnabled:$false
