128b3bf0ae2bb0d0ca290467c3a087a137b1ff8e
Dimitrios Bampas <dimitrios.bampas@thenavpeople.co.uk>
2020-11-08 21:14:59 +0200

    Change

--- Office 365/Make Team Calendar Visible in Outlook.ps1
+++ Office 365/Make Team Calendar Visible in Outlook.ps1
@@ -2,4 +2,4 @@
 #https://docs.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
 Connect-ExchangeOnline -UserPrincipalName db@all4nav.gr -ShowProgress $true
 #Make team's calendar available in Outlook.
-set-UnifiedGroup -identity "Γενική Διεύθυνση - Τεστ" -HiddenFromExchangeClientsEnabled:$false
+set-UnifiedGroup -identity <Team name> -HiddenFromExchangeClientsEnabled:$false
